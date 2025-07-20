f0ed38c162935b2334b9392df462cc9b838ac51b
Danila Rybin <rybin.danila@gmail.com>
2025-07-20 23:47:28 +0300

    Доработать задание по ТЗ спринта #6

--- src/ru/yandex/javacourse/history/InMemoryHistoryManager.java
+++ src/ru/yandex/javacourse/history/InMemoryHistoryManager.java
@@ -2,23 +2,61 @@ package ru.yandex.javacourse.history;
 
 import ru.yandex.javacourse.tasks.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-
-    private final List<Task> history = new ArrayList<>();
+    private final Map<Integer, Node<Task>> nodeMap = new HashMap<>();
+    private Node<Task> head;
+    private Node<Task> tail;
 
     @Override
     public void add(Task task) {
-        history.add(task);
-        if (history.size() > 10) {
-            history.remove(0);
+        if (task == null) return;
+
+        remove(task.getId()); // удалим, если уже есть
+        linkLast(task);       // добавим в конец
+    }
+
+    private void linkLast(Task task) {
+        final Node<Task> newNode = new Node<>(tail, task, null);
+        if (tail != null) {
+            tail.next = newNode;
+        } else {
+            head = newNode;
+        }
+        tail = newNode;
+        nodeMap.put(task.getId(), newNode);
+    }
+
+    @Override
+    public void remove(int id) {
+        final Node<Task> node = nodeMap.remove(id);
+        if (node == null) return;
+
+        final Node<Task> prev = node.prev;
+        final Node<Task> next = node.next;
+
+        if (prev != null) {
+            prev.next = next;
+        } else {
+            head = next;
+        }
+
+        if (next != null) {
+            next.prev = prev;
+        } else {
+            tail = prev;
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        List<Task> history = new ArrayList<>();
+        Node<Task> current = head;
+        while (current != null) {
+            history.add(current.data);
+            current = current.next;
+        }
+        return history;
     }
 }
